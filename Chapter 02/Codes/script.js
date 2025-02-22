59724d3a4180f4d29a49ad8ed86cdf198a9c190f
Diku Mardi <dikumardi047@gmail.com>
2025-02-22 20:37:35 +0530

    add chapter 2

---
+++ Chapter 02/Codes/script.js
@@ -0,0 +1,194 @@
+/* Variable */
+// firstname = "Maaike";
+// x = 2;
+
+
+// let firstname = "Maria";
+// firstname = "Jacky";
+// console.log(firstname);
+
+
+/*
+ Primitive data types
+-JavaScript has seven primitives:1. String,
+                                 2. Number, 
+                                 3. BigInt, 
+                                 4. Boolean, 
+                                 5. Symbol, 
+                                 6. undefined, and 
+                                 7. null. 
+*/
+// String,
+/* A string is used to store a text value. It is a sequence of characters. There are different 
+ways to declare a string:
+• Double quotes
+• Single quotes
+• Backticks: special template strings in which you can use variables directly
+*/
+
+// let singleString = 'Hi there!';
+// let doubleString = "How are you";
+
+// let funActivity = "Let's learn JavaScript";
+// console.log(funActivity);
+
+// let question = "Do you want to learn JavaScript? "Yes!"";
+//  let ans = 'ds ds  aasx "ddsa" ';
+//  console.log(ans);
+ 
+//  let aww = `Let's  mewow mewo mewo mewo`;
+
+// Backticks
+/*
+let p_language = "JS" ;
+let message = `Let's Learn ${p_language}`;
+console.log(message);
+*/
+
+/* Escape characters 
+
+let str = "Hello, what's your name? Is it \"Mike\"?";
+let str2 = 'Hello, what\'s your name? Is it "Mike"?';
+
+MORE USES OF \ BACKSLASH IN JS
+let str3 = "New \n line.";
+let str4 = "I'm containing a backslash : \\";
+console.log(str3);
+console.log(str4);
+*/
+
+
+
+ 
+/* Number 
+ let intNr = 1 ;
+ let decNr = 1.5 ;
+ let expNr = 1.4e15;
+console.log(expNr);
+
+OCTAL NUMBER:
+ let octr = 0o10;
+ console.log(octr);
+ 
+ HEXIAL:
+ let hexNr = 0x3E8;
+ console.log(hexNr);
+ 
+ BINARY:
+ let binNr = 0b101; 
+ console.log(binNr);
+
+        NUMBERS INTEGER,NEGATIVE,DECIMAL,BINARY... 
+       
+        1.integer
+         let intNr2 = 325;
+
+       2. negative
+        let intNr3=-785;
+
+        3.decimal
+        let decNr2 = 12.232;
+
+        4.binary
+        let binNr2 = 0b100 ;
+
+       4. binary
+        let binNr3 = 0b1110 ;
+        console.log(binNr3);
+
+
+        ADDING  bigInt and integer
+        let bigNr = 90071992547409920n;
+         let intNr = 25441;
+
+        let result = bigNr + intNr;
+
+        console.log(result);
+
+
+        // BOOLEAN - true and false
+ 
+        let doorclose = true;
+
+        let lightoff = false ;
+        
+        SYMBOL:
+        let str1 = "JavaScript is fun!";
+let str2 = "JavaScript is fun!"
+console.log("These two strings are the same:", str1 === str2);
+
+let str3 = Symbol("JavaScript is fun!");
+let str4 = Symbol("JavaScript is fun!");
+console.log("These two strings are the same:", str3 === str4);
+    
+
+//UNDEFINED
+let notAssignValue ;
+console.log(notAssignValue);
+
+
+      NULL
+      let empty = null; 
+
+      see undefined and null difference
+     good pratice  and bad pratice 
+            let badpratice = undefined ;
+ let lastname ;
+ console.log("Same Undefined:", lastname === badpratice);
+
+ let ucanusenull = null ;
+ console.log("Same null:", lastname === ucanusenull);
+ 
+ 
+  WORKING OUT THE TYPE OF A VARIABLE
+           testVariable = 1.2 ;
+           variableTypeTest1 = typeof testVariable ;
+           variableTypeTest2 = typeof(testVariable);
+           console.log(variableTypeTest1);
+           console.log(variableTypeTest2);   
+      
+
+
+                      let str = "hello";
+console.log("str",typeof str); // string
+
+let no = 7;
+console.log("no",typeof no); // number
+
+let bigno = 12345678901234n;
+console.log("big-no",typeof bigno); // bigInt
+
+let bool = true;
+console.log("bool",typeof bool); // boolean
+
+let sym = Symbol("unique");
+console.log("Symbol",typeof sym); //symbol
+
+let undef = undefined;
+console.log("undefined value",typeof undef); // undefined
+
+let unknow = null;
+console.log("unknow ",typeof unknow); // object
+*/
+
+/*
+ CONVERTING DATA TYPES
+
+* */
+
+
+
+
+
+
+
+
+
+         
+
+
+
+
+ 
+
+
