6a88294f4170a16e63431c44b12bd5a096c55742
Diku Mardi <dikumardi047@gmail.com>
2025-07-11 20:50:40 +0530

    added new js file

---
+++ Chapter 02/Exercise_2.1/script.js
@@ -0,0 +1,24 @@
+/* Practice exercise 2.1
+ What are the types of these variables listed below? Verify this with typeof and 
+output the result to the console: */
+
+ let str1 = 'Laurence'; // type is 'string' & output is Laurence
+let str2 = "Svekis"; //  type is  'string' & output is Svekis
+let val1 = undefined; // type is 'undefined' & output is undefined
+ let val2 = null; // type is 'object' & output is null 
+ let myNum = 1000; // type is 'number' & output is 1000
+
+ console.log(typeof(str1));
+ console.log(str1);
+
+  console.log(typeof(str2));
+ console.log(str2);
+
+  console.log(typeof(undefined));
+ console.log(undefined);
+
+ console.log(typeof(null));
+ console.log(null);
+
+  console.log(typeof(myNum));
+ console.log(myNum);
---
+++ Chapter 02/Exercise_2.2/script.js
@@ -0,0 +1,9 @@
+/*Create a variable for your name, another one for your age, and another one for 
+whether you can code JavaScript or not.
+ Log to the console the following sentence, where name, age and true/false are 
+variables:  */
+
+const name = 'Maaike' ; 
+let age = 29 ;
+let cancode = true ;
+console.log('Hello, my name is ',name , 'I am ', age , 'and i can code javascript :' , cancode);
