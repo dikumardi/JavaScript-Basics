54d1a8ebec9c2644d441ffbe85c4629d307c891a
Diku Mardi <dikumardi047@gmail.com>
2025-07-12 21:11:01 +0530

    new js file added

---
+++ Chapter 02/Exercise_2.4/script.js
@@ -0,0 +1,18 @@
+/* Create variables for three numbers: a, b, and c. Update these variables with the 
+following actions using the assignment operators:
+ • Add b to a
+ • Divide a by c
+   • Replace the value of c with the modulus of c and b
+ • Print all three numbers to the console
+ */
+let  a = 10 ;
+let  b = 20 ;
+let  c = 30 ;
+ a = b + a ;
+ a = a / c ; 
+c= c% b ;
+ console.log(a,b,c);
+
+ 
+ 
+ 
