6fcc710e1fc5041432c1c2fe7bd5efc1221d0f6e
Diku Mardi <dikumardi047@gmail.com>
2025-07-12 21:10:43 +0530

    new js file added Exercise_2.3

---
+++ Chapter 02/Exercise_2.3/script.js
@@ -0,0 +1,3 @@
+/** Write some code to calculate the hypotenuse of a triangle using the Pythagorean 
+theorem when given the values of the other two sides. The theorem specifies that the 
+relation between the sides of a right-angled triangle is a2 + b2 = c2 */
