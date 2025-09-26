753df89d6be287c1737b2b81391b73d4921fb6d7
Diku Mardi <dikumardi047@gmail.com>
2025-09-26 18:39:00 +0530

    update

--- Chapter 02/Exercise_2.3/script.js
+++ Chapter 02/Exercise_2.3/script.js
@@ -1,3 +1,7 @@
 /** Write some code to calculate the hypotenuse of a triangle using the Pythagorean 
 theorem when given the values of the other two sides. The theorem specifies that the 
 relation between the sides of a right-angled triangle is a2 + b2 = c2 */
+let a = prompt("Enter a");
+let b = prompt("Enter b");
+let hypotenuse = Math.sqrt(a * a + b * b);
+console.log(hypotenuse);
